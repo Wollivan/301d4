8de2296ff2568bae33078a5abd12bf1e0c80c9b9
Tim Smith <tim.smith2814@gmail.com>
2023-05-11 10:02:59 +0100

    Lab 8 demo

---
+++ class-08/lab/demo/server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = 8080;
+const app = express();
+app.use(cors());
+
+const data = require("./data/weather.json");
+
+app.get("/", (request, response) => {
+  response.json("You are looking at the root route.");
+});
+
+app.get("/weather", (request, response) => {
+  const { lat, lon, searchQuery } = request.query;
+
+  const city = data.find((item) => {
+    return item.city_name === searchQuery; // && item.lat === lat && item.lon === lon;
+  });
+
+  const forecasts = [];
+  city.data.forEach((day) => {
+    const fc = { date: day.valid_date, description: day.weather.description };
+    forecasts.push(fc);
+  });
+
+  response.json(forecasts);
+});
+
+app.listen(PORT, () => console.log(`App is running on PORT ${PORT}`));
